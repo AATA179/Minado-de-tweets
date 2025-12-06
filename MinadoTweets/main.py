5ecbaa0013fb626e19efd180caaabc842bced766
AATA179 <158798009+AATA179@users.noreply.github.com>
2025-12-05 18:09:24 -0600

    Update main.py

--- MinadoTweets/main.py
+++ MinadoTweets/main.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
     ct0 = "ct0"        # ct0 sacado del navegador
     
     query = "búsqueda" # Palabra o hashtag a buscar
-    cantidad = 5 # Número de tweets a extraer de 5 a 30
+    cantidad = 5 # Número de tweets a extraer de 5 a 500
     
     # Crear un cliente autenticado con las llaves ingresadas
     cliente = auth.crear_cliente(auth_token, ct0)
     
     # Ejecutar el programa de forma asincrónica
+
     asyncio.run(extraer.buscar_tweets(cliente, query, cantidad))
